656d8cc7abb730b17a3312eb079b35518aaaf2ab
ZTattempt <62923868+noah-forest@users.noreply.github.com>
2023-08-20 21:10:13 -0500

    created base classes

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+	public Unit unitToAttack;
+	
+	//private Actor actor; -- tbd
+	
+	private void AttackUnit()
+	{
+		// attack the unit to attack
+		
+	}
+
+	private void Damage(int dmg)
+	{
+		// deal this amount of damage
+	}
+
+	private void OnDied()
+	{
+		// go to grave
+	}
+
+	private void Respawn()
+	{
+		// come back from grave
+	}
+}
