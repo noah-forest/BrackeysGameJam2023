a8292ed7abb730039403eeb0e9ec924ad3688903
ZTattempt <62923868+noah-forest@users.noreply.github.com>
2024-07-10 08:19:32 -0500

    main menu changes

---
+++ Assets/Scripts/UI/MainMenu/ShowText.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowText : MonoBehaviour
+{
+	public GameObject text;
+
+	public void TextEnable()
+	{
+		text.SetActive(true);
+	}
+}
