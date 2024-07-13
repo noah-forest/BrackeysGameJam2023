566d4cd7d959844669cc125ae85792dc1daeb52e
ZTattempt <62923868+noah-forest@users.noreply.github.com>
2024-07-13 08:42:32 -0500

    font rotator now works while paused

--- Assets/Scripts/UI/Text/FontRotator.cs
+++ Assets/Scripts/UI/Text/FontRotator.cs
@@ -1,27 +1,48 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class FontRotator : MonoBehaviour
 {
     public TMP_FontAsset[] fonts;   // List of fonts to rotate between
     public float framesPerSecond = 10f;   // Time interval between font changes (in seconds)
 
+	public bool stopOnPause;
+
     private TMP_Text textMeshPro;
     private int currentFontIndex = 0;
 
-    private void Start()
+	private void Awake()
+	{
+		textMeshPro = GetComponent<TMP_Text>();
+
+		if (fonts.Length > 0)
+		{
+			textMeshPro.font = fonts[currentFontIndex];
+		}
+	}
+
+	private void OnEnable()
+	{
+		StartCoroutine(nameof(RotateFont));
+	}
+
+
+	private IEnumerator RotateFont()
     {
-        textMeshPro = GetComponent<TMP_Text>();
-        if (fonts.Length > 0)
-        {
-            textMeshPro.font = fonts[currentFontIndex];
-            // Start the animation loop
-            InvokeRepeating("RotateFont", 0f, 1f / framesPerSecond);
+		while (true)
+		{
+			currentFontIndex = (currentFontIndex + 1) % fonts.Length;
+			textMeshPro.font = fonts[currentFontIndex];
+
+			if (stopOnPause)
+			{
+				yield return new WaitForSeconds(1f / framesPerSecond);
+			}
+            else
+            {
+                 yield return new WaitForSecondsRealtime(1f / framesPerSecond);
+            }
         }
-    }
-    private void RotateFont()
-    {
-        currentFontIndex = (currentFontIndex + 1) % fonts.Length;
-        textMeshPro.font = fonts[currentFontIndex];
     }
 }
