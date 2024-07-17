1fceaf50184562b6dffb6de03ac523d56512b811
ZTattempt <62923868+noah-forest@users.noreply.github.com>
2024-07-17 07:55:39 -0500

    decoupled lock/refresh scripts

--- Assets/Scripts/UI/Shop/LockShop.cs
+++ Assets/Scripts/UI/Shop/LockShop.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LockShop : MonoBehaviour
 {
-    public GameObject openLock;
-    public GameObject closedLock;
+	public GameObject openLock;
+	public GameObject closedLock;
 	public GameObject refreshLock;
 	public GameObject refreshCost;
 
@@ -17,25 +14,22 @@ public class LockShop : MonoBehaviour
 	private Sprite defaultBorderTexture;
 
 	public Button refreshButton;
-	public RefreshShop refreshShop;
 
-    public bool locked;
+	public bool locked;
 
 	private GameManager gameManager;
-    
-    private void Start()
-    {
+
+	private void Start()
+	{
 		gameManager = GameManager.singleton;
 
 		defaultBorderTexture = lockBorder.sprite;
 
-		refreshShop = refreshButton.gameObject.GetComponent<RefreshShop>();
-
-        Button button = GetComponent<Button>();
-        button.onClick.AddListener(SetShopLocked);
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(SetShopLocked);
 
 		gameManager.startGame.AddListener(UnlockShop);
-    }
+	}
 
 	private void OnEnable()
 	{
@@ -46,10 +40,10 @@ public class LockShop : MonoBehaviour
 	}
 
 	private void SetShopLocked()
-    {
+	{
 		if (!locked) LockShopItems();
 		else UnlockShop();
-    }
+	}
 
 	private void LockShopItems()
 	{
@@ -66,10 +60,10 @@ public class LockShop : MonoBehaviour
 		gameManager.shopLocked.Invoke();
 	}
 
-    private void UnlockShop()
-    {
-        locked = false;
-        openLock.SetActive(true);
+	private void UnlockShop()
+	{
+		locked = false;
+		openLock.SetActive(true);
 		refreshCost.SetActive(true);
 		closedLock.SetActive(false);
 		refreshLock.SetActive(false);
--- Assets/Scripts/UI/Shop/RefreshShop.cs
+++ Assets/Scripts/UI/Shop/RefreshShop.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 public class RefreshShop : MonoBehaviour
 {
 	public ShopController shopController;
-	public LockShop lockShop;
 
 	public TextMeshProUGUI refreshCostText;
 
