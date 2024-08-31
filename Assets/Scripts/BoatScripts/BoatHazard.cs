73b9a224fc116d7efaea76f4edf6412a9bf4d96d
WHHWII <36775534+WHHWII@users.noreply.github.com>
2024-08-31 13:01:21 -0500

    boat scene

--- Assets/Scripts/BoatScripts/BoatHazard.cs
+++ Assets/Scripts/BoatScripts/BoatHazard.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BoatHazard : MonoBehaviour, IBoatInteractable
 {
     public void InteractWithBoat(BoatMaster boat)
