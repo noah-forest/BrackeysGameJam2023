a7fda9bffe1300a7e057815c5cc3778524fa80b5
WHHWII <36775534+WHHWII@users.noreply.github.com>
2024-09-24 11:12:36 -0500

    boat hazard sounds

--- Assets/Scripts/BoatScripts/BoatHazard.cs
+++ Assets/Scripts/BoatScripts/BoatHazard.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 
 public class BoatHazard : MonoBehaviour, IBoatInteractable
 {
-    protected BoatWorldManager boatManager;
+    protected static BoatWorldManager boatManager;
+    protected static AudioSource soundPlayer;
     [SerializeField] protected GameObject impactEffect;
+    [SerializeField] protected AudioClip impactSound;
+    [SerializeField] protected float impactVolume = 5;
     /// <summary>
     /// The higher this value, the more 'difficult' it will be for the path to go through this hazard, however 0 will make the tile impassible
     /// </summary>
     public float pathFindingInfluence = 1;
+    
+
 
     private void Start()
     {
-        boatManager = BoatWorldManager.singleton;
+        if(!boatManager) boatManager = BoatWorldManager.singleton;
+        if(!soundPlayer) soundPlayer = GameManager.singleton?.MusicPlayer;
+
     }
     public virtual void InteractWithBoat(BoatMaster boat, ControllerColliderHit hit)
     {
@@ -25,6 +32,7 @@ public class BoatHazard : MonoBehaviour, IBoatInteractable
         boatManager.boatLaneSpawn = boat.controller.CurrentLane;
         //Debug.Log($"Boat impacted {gameObject.name}");
         if(impactEffect) Instantiate(impactEffect, transform.position, transform.rotation);
+        soundPlayer?.PlayOneShot(impactSound, impactVolume);
 
     }
 }
